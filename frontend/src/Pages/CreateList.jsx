8c5c34e0331825aab43a38999542f9df2734d3d0
Shivraj Muley <126469673+shivrajmuley@users.noreply.github.com>
2024-07-17 18:48:33 +0530

    Update CreateList.jsx

--- frontend/src/Pages/CreateList.jsx
+++ frontend/src/Pages/CreateList.jsx
@@ -145,7 +145,7 @@ const CreateList = (active) => {
   console.log(creator);
 
   //fetching data
-if(email != null){
+
   const createListHandle = (e) => {
     e.preventDefault();
     console.log(imgFile);
@@ -181,10 +181,7 @@ if(email != null){
         toast.success("New Home Created");
       })
       .catch((error) => toast.error("Something went wrong "));
-  }
-else{
-   toast.error("Please login first  ")
-}
+
     // Error Handle
 
     setFormErr(true);
@@ -642,7 +639,7 @@ else{
 
       <button
         onClick={
-        createListHandle
+         email != null ? createListHandle : toast.error("Please Login First ")
         }
         className="bg-green-600 text-white text-xl font-semibold px-12 rounded-md py-4 ml-16 mt-8 mb-28 tracking-wider hover:bg-green-950 phones:text-base phones:ml-8"
       >
