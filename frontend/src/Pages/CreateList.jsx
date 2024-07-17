13359327ff83ffe63b059bf6ac8a335d17f48b45
Shivraj Muley <126469673+shivrajmuley@users.noreply.github.com>
2024-07-17 08:29:28 +0530

    Update CreateList.jsx

--- frontend/src/Pages/CreateList.jsx
+++ frontend/src/Pages/CreateList.jsx
@@ -633,9 +633,7 @@ const CreateList = (active) => {
       </div>
 
       <button
-        onClick={
-          email != null ? createListHandle : toast.error("Please Login First ")
-        }
+        onClick={createListHandle}
         className="bg-green-600 text-white text-xl font-semibold px-12 rounded-md py-4 ml-16 mt-8 mb-28 tracking-wider hover:bg-green-950 phones:text-base phones:ml-8"
       >
         Create your listing
